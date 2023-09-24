69e598e456c51fb048765f4c27a8f042b13cddbe
unknown <auditor.kbfg@gmail.com>
2023-09-24 16:21:38 +0900

    check_MISC_in_PATH_and_make_original_path

--- .00.python.bat
+++ .00.python.bat
@@ -1,4 +1,13 @@
 @echo off
+chcp 65001
+
+rem PATH 환경 변수에 MISC_CSPM이 포함되어 있는지 확인합니다.
+echo %PATH% | findstr /C:"MISC_CSPM" > nul
+if %errorlevel% equ 0 (
+    echo PATH 환경 변수에 MISC_CSPM이 포함되어 있습니다. 스크립트를 실행하지 않습니다.
+    pause
+    exit /b
+)
 
 rem 배치 파일이 위치한 폴더 경로를 상대 경로로 파악합니다.
 for %%I in ("%~dp0") do set "parent_dir=%%~fI"
@@ -7,6 +16,9 @@ rem PATH에 새로운 path 셋팅
 
 rem %parent_dir%Scripts 값을 %scripts_path% 변수에 저장합니다.
 set "scripts_path=%parent_dir%python3\Scripts"
+
+rem 기존 path를 일단 저장
+echo %PATH% > "%parent_dir%\original_path.txt"
 
 rem 기존의 PATH에 %scripts_path%를 추가합니다.
 set "PATH=%PATH%;%python_path%"
